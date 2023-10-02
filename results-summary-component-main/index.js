dd050feff4a5d19a703e66c14c526287f2155966
Joshua Dancey <joshuadancey@hotmail.com>
2023-10-02 10:27:09 +0100

    working on corner borders for segment, working on inserting average, working on readme

--- results-summary-component-main/index.js
+++ results-summary-component-main/index.js
@@ -1,10 +1,19 @@
 import data from "./data.json" assert { type: "json" };
 
 const allSummarySegments = document.getElementsByClassName("summary-segment");
-console.log(allSummarySegments);
+const scoreAverageP = document.querySelector("#score-average");
+
+const getAverage = (dataArray) => {
+  if (!dataArray || dataArray.length === 0) return 0;
+  const totalScore = dataArray.reduce(
+    (accumulator, currentValue) => (accumulator += currentValue.score),
+    0
+  );
+  return Math.round(totalScore / dataArray.length).toString();
+};
+const scoreAverage = getAverage(data);
 
 Array.from(allSummarySegments).forEach((segment, index) => {
-  console.log(segment);
   const image = segment.querySelector("img");
   if (image) image.src = data[index].icon;
   const categoryName = segment.querySelector("p");
@@ -12,3 +21,5 @@ Array.from(allSummarySegments).forEach((segment, index) => {
   const score = segment.querySelector("span");
   score.innerText = data[index].score;
 });
+
+scoreAverageP.innerText = scoreAverage;
