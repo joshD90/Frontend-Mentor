a4f3b5b3cea05380a71f204a327ae709d4b21bd3
Joshua Dancey <joshuadancey@hotmail.com>
2023-11-21 23:48:49 +0000

    changing picture to background image to allow to fit to grid, changing js file to reflect not needing to change the image, adding in more responsiveness for the inset grid

--- news-homepage-main/index.js
+++ news-homepage-main/index.js
@@ -1,6 +1,7 @@
 const burgerMenu = document.querySelector(".nav-burger");
 const navItems = document.querySelector(".nav-items");
 const navExit = document.getElementById("exit-nav-items");
+// const shapesImage = document.getElementById("shapesImg");
 
 burgerMenu.addEventListener("click", () => {
   if (window.innerWidth > 768) return;
@@ -11,3 +12,17 @@ navExit.addEventListener("click", () => {
   if (window.innerWidth > 768) return;
   navItems.style.display = "none";
 });
+
+// const setImg = () => {
+//   console.log(shapesImage);
+//   const imgSrc =
+//     window.innerWidth < 768
+//       ? "./assets/images/image-web-3-mobile.jpg"
+//       : "./assets/images/image-web-3-desktop.jpg";
+//   ("./assets/images/image-web-3-desktop.jpg");
+//   shapesImage.src = imgSrc;
+// };
+// window.addEventListener("load", () => setImg());
+// window.addEventListener("resize", () => {
+//   setImg();
+// });
